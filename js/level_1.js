e83941594c2dfd17cb758f75d51a45ed374cc988
catsolutions1 <143562269+catsolutions1@users.noreply.github.com>
2025-10-08 09:46:07 -0700

    load header and footer using js

---
+++ js/level_1.js
@@ -0,0 +1,10 @@
+document.getElementById("header")
+    .innerHTML =
+        `<h1 class="header-title">catsolutions's website</h1>
+        <a href="./">Home</a>
+        <span style="display: inline-block;"></span>
+        <a href="./articles">Articles</a>`;
+
+document.getElementById("footer")
+    .innerHTML =
+        `<p><a href="#header">jump to top</a></p>`;
